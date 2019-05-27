3ba5a1d858fa96f608f12b8796a8292f666fbc24
willy_kid <nillyw31@gmail.com>
2019-05-27 12:21:50 +0300

    registration template done

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,6 @@ def home(request):
 
     return render(request, "users/home.html")
 
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form' : form})
