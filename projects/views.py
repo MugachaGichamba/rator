3480fc270732c8f2f744af2c4956b07bf084c9c3
willy_kid <nillyw31@gmail.com>
2019-05-29 12:16:47 +0300

    imported relevant imports in project views

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import  CreateView
+from .models import Project
+
