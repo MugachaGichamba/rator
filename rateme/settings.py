a2e652af5965d38fd40e281347cdeadfeeb53740
willy_kid <nillyw31@gmail.com>
2019-05-29 10:16:50 +0300

    added static and media settings in debug mode

--- rateme/settings.py
+++ rateme/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
