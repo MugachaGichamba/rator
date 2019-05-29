fc5a76a73af72024198860a31b56821197ad3b8e
willy_kid <nillyw31@gmail.com>
2019-05-29 12:46:39 +0300

    added absolute url to project model

--- projects/models.py
+++ projects/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Project(models.Model):
     title = models.CharField(max_length=100)
@@ -17,3 +17,6 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('home')
--- rateme/urls.py
+++ rateme/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('update_profile/', user_views.update_profile, name="update"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
-    # path('', include('project.urls')),
+    path('', include('projects.urls')),
 ]
 
 if settings.DEBUG:
