014be4bab602077de1598d86652d8d8a2a2d0973
willy_kid <nillyw31@gmail.com>
2019-05-29 12:19:08 +0300

    project view done

--- projects/views.py
+++ projects/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import  CreateView
 from .models import Project
+from django.contrib import messages
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    fields = ['title', 'image', 'description', 'link']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, "Image successfully uploaded")
+        return super().form_valid(form)
 
