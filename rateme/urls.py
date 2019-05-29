2ccecc0815b7f5c5cd8214a7d2818867804bc52a
willy_kid <nillyw31@gmail.com>
2019-05-29 08:58:30 +0300

    created login path in urls

--- rateme/urls.py
+++ rateme/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', user_views.home, name="home"),
     path('register/', user_views.register, name="register"),
+    path('login/', auth_views.LoginView, name="login"),
+    # path('', include('project.urls')),
 ]
