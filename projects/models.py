1d40ab25ff1b2e5ee41f158b2aa488efa905eda1
willy_kid <nillyw31@gmail.com>
2019-05-29 12:12:49 +0300

    created projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(blank=False, upload_to='home_pics')
+    description = models.TextField()
+    link = models.URLField(
+        max_length=128,
+        db_index=True,
+        unique=True,
+        blank=True
+    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
