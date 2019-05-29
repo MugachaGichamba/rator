4e2833e4d7e348290d4f2a34119c78eca3e95ff6
willy_kid <nillyw31@gmail.com>
2019-05-29 09:36:11 +0300

    profile template done

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 # Create your views here.
 def home(request):
@@ -24,5 +23,7 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
